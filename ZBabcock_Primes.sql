69143e6b438f61627d2afe484a881c2975bef836
babbincocks <zabbybab@gmail.com>
2017-12-18 17:25:18 -0500

    Started Prime queries.

---
+++ ZBabcock_Primes.sql
@@ -0,0 +1,21 @@
+CREATE PROC sp_PrimeGen
+AS
+BEGIN
+
+	DECLARE @Counter INT
+	DECLARE @Limit INT
+	SET @Counter = 1
+	SET @Limit = ''
+	
+	WHILE @Counter <= @Limit
+		BEGIN
+			
+
+
+
+
+
+		END
+
+	SET @Counter = @Counter + 1
+END
