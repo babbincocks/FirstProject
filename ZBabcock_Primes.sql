b5f725be550dbd1d74812b6e6d33fd32f3e1d728
babbincocks <zabbybab@gmail.com>
2017-12-18 19:31:18 -0500

    Not much progress.

--- ZBabcock_Primes.sql
+++ ZBabcock_Primes.sql
@@ -1,29 +1,37 @@
 CREATE PROC sp_PrimeGen
-(	@Counter INT = 1,
-	 @Limit INT = 1,
-	 @Prime INT = 1
+(	
+	 @Limit INT = ''
+	
 )
 AS
 BEGIN
-	
-	WHILE @Counter <= @Limit
-		
-			
+	DECLARE @Counter INT = 2
+	DECLARE @Prime BIT = 1
+	WHILE @Limit > @Counter 
+	BEGIN	
+		IF  @Limit % @Counter = 0
 		BEGIN
-
-			PRINT @Prime
-
-		
-			SET @Counter = @Counter + 1
+			SET @Prime = 0
+			BREAK
 		END
-
+		
+		SET @Counter = @Counter + 1
+	END
+			
+			
+	IF @Prime <> 0
+	BEGIN
+		PRINT @Counter
+	END
 
 END
 
 
 
 
-EXEC sp_PrimeGen @Counter = 1 , @Limit = 4 
+EXEC sp_PrimeGen  @Limit = 10
+
+
 
 DROP PROC sp_PrimeGen
 
@@ -31,3 +39,40 @@ GO
 
 
 
+--DECLARE @TESTVALUE BIGINT = 233
+--DECLARE @DIVISOR BIGINT = 2
+--DECLARE @PRIME BIT = 1
+
+--WHILE @DIVISOR < (@TESTVALUE / 2)
+--BEGIN
+--	IF (@TESTVALUE % @DIVISOR = 0)  -- If the remainder of the two divided (modulus) is 0.
+--	BEGIN
+--		PRINT 'The number is not prime. It is divisible by ' + CAST(@DIVISOR AS VARCHAR(10))
+--		SET @PRIME = 0
+--		BREAK	
+--	END
+--	SET @DIVISOR = @DIVISOR + 1
+--END	
+
+--IF @PRIME <> 0
+--	PRINT CAST(@TESTVALUE AS VARCHAR(10)) + ' is a prime number.'
+
+
+
+
+
+
+
+
+--create table prime (primeno bigint)
+--declare @counter bigint
+--set @counter = 2
+--while @counter < 1000000
+--begin
+--if not exists(select top 1 primeno from prime where @counter % primeno = 0 )
+--	insert into prime 
+--	select @counter
+--	set @counter = @counter + 1
+--end
+
+--select * from prime order by 1
